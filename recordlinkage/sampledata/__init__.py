980319e139719b8b38236a9a97877ed8695759cd
jonathandebruin <jonathandebruinhome@gmail.com>
2015-11-28 08:57:04 +0100

    New way of generating the second data file + Example data

--- recordlinkage/sampledata/__init__.py
+++ recordlinkage/sampledata/__init__.py
@@ -8,15 +8,20 @@ try:
 except Exception:
 	print 'Faker is not installed. Therefore, the functionalities of this module are limited.'
 
-def addtypos():
+personaldata1000A = pd.read_csv('data/personaldata1000A.csv', sep=';')
+personaldata1000B = pd.read_csv('data/personaldata1000B.csv', sep=';')
+
+def addtypos(df):
 
 	return
 
-def addsubstitutions():
+def addsubstitutions(df):
 
 	return
 
 def fakeperson():
+
+	fake = Faker()
 
 	person = {}
 
@@ -38,21 +43,51 @@ def fakeperson():
 
 	return person
 
-def dataset(N):
+def dataset(N, df=None, matches=None):
 
-	try:
-		fake = Faker()
-	except:
-		return pd.read_csv('data/censusdataA.csv')
+	if df is None:
 
-	try:
+		# Create a dataframe with fake personal information
+		df_persons = pd.DataFrame([fakeperson() for _ in range(0, N)])
 
-		for _ in range(0,N):
+		# Set an entity id for each created record. 
+		df_persons['entity_id'] = np.arange(1, N+1)
+
+		# Reset the index of the dataframe. Start at 1e6
+		df_persons.index = np.arange(1e6, 1e6+N)
+
+		# Return the dataframe
+		return df_persons
+
+	elif df is not None:
+
+		len_df = N-matches
+		if len_df < 0:
+			raise ValueError("The number of matches is higher than the number of records N.")
+
+		# Create a dataframe with fake personal information
+		df_persons = pd.DataFrame([fakeperson() for _ in range(0, len_df)])
+
+		# Set an entity id for each created record. 
+		max_entity_id = max(df['entity_id'])
+		df_persons['entity_id'] = np.arange(max_entity_id+1, max_entity_id+len_df+ 1)
+
+		# Dataframe
+		df_persons = df.sample(matches).append(pd.DataFrame([fakeperson() for _ in range(0, len_df)]))
+
+		# Reset the index of the dataframe. Start at 1e6
+		df_persons.index = np.arange(1e6, 1e6+N)
+
+		# Return the dataframe
+		return df_persons
+
+# censusdataA = dataset(1000)
+# censusdataB = dataset(1000, censusdataA, 800)
+# print censusdataA.head()
+# print censusdataB.head()
+
+# censusdataA.to_csv('data/personaldata1000A.csv', sep=';')
+# censusdataB.to_csv('data/personaldata1000B.csv', sep=';')
 
 
 
-			print fake.name()
-
-	return None
-
-censusdataA = dataset()
