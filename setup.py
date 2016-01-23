2fda98e9792dc71620d1e873b468d380ce5af87c
jonathandebruin <jonathandebruinhome@gmail.com>
2016-01-23 21:42:22 +0100

    Add support for static files in install

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import versioneer
 
 setup(
@@ -6,9 +6,13 @@ setup(
 	version=versioneer.get_version(),
 	cmdclass=versioneer.get_cmdclass(),
   	author="Jonathan de Bruin",
+  	author_email="jonathandebruinhome@gmail.com",
 	platforms="any",
 	description="A tool to link or deduplicate small or medium sized datasets.",
 	url="https://github.com/J535D165/recordlinkage",
 	install_requires=["pandas", "numpy", "jellyfish"],
-	packages=["recordlinkage", "recordlinkage.standardise", "recordlinkage.datasets"]
-	)
+	packages=['recordlinkage', 'recordlinkage.datasets', 'recordlinkage.standardise'],
+	include_package_data=True,
+	package_dir={'recordlinkage': 'recordlinkage'},
+	package_data={'recordlinkage': ['datasets/data/*.csv']},
+)
