672ac4228cec6b8335befb41ba5b66d5053c3525
jonathandebruin <jonathandebruinhome@gmail.com>
2016-04-10 08:29:08 +0200

    Generate latexpdf in sphinx on build

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 # Run in terminal with: sh build.sh
 
+# Create the documentation.
 cd docs/
 jupyter nbconvert --to rst ../examples/*.ipynb
 
 make html
+make latexpdf
 
+# Make the package installable. 
 cd ..
-
 python setup.py sdist
 python setup.py build
 python setup.py install
