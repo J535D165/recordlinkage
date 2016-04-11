58a3e04e90a7e7a10e7fefef7a21e96f83d19a47
jonathandebruin <jonathandebruinhome@gmail.com>
2016-04-11 09:04:36 +0200

    Use mock

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,12 @@ sys.path.insert(0, os.path.abspath('..'))
 
 import recordlinkage
 
+import mock
+ 
+MOCK_MODULES = ['numpy', 'scipy', 'pandas', 'sklearn']
+for mod_name in MOCK_MODULES:
+sys.modules[mod_name] = mock.Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
