0d439e2b1574bebfc09dc37cbdf6ae9db544a65b
jonathandebruin <jonathandebruinhome@gmail.com>
2016-04-16 10:44:48 +0200

    Update documentation, rename arguments and update performance

--- recordlinkage/comparing.py
+++ recordlinkage/comparing.py
@@ -1,41 +1,114 @@
 from __future__ import division 
 
-import pandas as pd
+import pandas
 import numpy as np
 
 from indexing import IndexError
 
 class Compare(object):
-	"""A class to make comparing of records fields easier. It can be used to compare fields of the record pairs. """
+	""" 
 
-	def __init__(self, pairs, A=None, B=None):
+	This class can be used to compare the attributes of candidate record pairs. This class has
+	several methods to compare data. 
 
-		self.A = A
-		self.B = B
+	:param pairs: The candidate record pairs. 
+	:param df_a: The first dataframe. 
+	:param df_b: The second dataframe.
+
+	:type pairs: pandas.MultiIndex
+	:type df_a: pandas.DataFrame
+	:type df_b: pandas.DataFrame
+
+	:returns: A compare class
+	:rtype: recordlinkage.Compare
+
+	:Example:
+
+	>>> comp = recordlinkage.Compare(candidate_pairs, census_data_1980, census_data_1990)
+	>>> comp.fuzzy('first_name', 'name', method='jarowinkler')
+	>>> comp.fuzzy('lastname', 'lastname', method='jarowinkler')
+	>>> comp.exact('dateofbirth', 'dob')
+	>>> comp.exact('sex', 'sex')
+	>>> comp.fuzzy('address', 'address', method='levenshtein')
+	>>> comp.exact('place', 'place')
+	>>> comp.numerical('income', 'income')
+	>>>
+	>>> print(comp.vectors.head())
+
+
+	 """
+
+	def __init__(self, pairs, df_a=None, df_b=None):
+
+		self.df_a = df_a
+		self.df_b = df_b
 		self.pairs = pairs
 
-		self.vectors = pd.DataFrame(index=pairs)
+		self.vectors = pandas.DataFrame(index=pairs)
 
-		self.ndim = self._compute_dimension(pairs)
+		# self.ndim = self._compute_dimension(pairs)
+
+	def compare(self, comp_func, data_a, data_b, name=None, store=True, *args, **kwargs):
+		"""
+
+		Core method to compare records. This method takes a function and data from both records in
+		the record pair. The data is compared with the compare function. The built-in methods also
+		use this function.
+
+		:param comp_func: A comparison function. This function can be a built-in function or a user defined comparison function.
+		:param data_a: The labels, Series or DataFrame to compare.
+		:param data_b: The labels, Series or DataFrame to compare.
+
+		:type comp_func: function
+		:type data_a: label, pandas.Series, pandas.DataFrame
+		:type data_b: label, pandas.Series, pandas.DataFrame
+
+		:return: The DataFrame Compare.vectors
+		:rtype: standardise.DataFrame
+		"""
+
+		args = list(args)
+
+		# Sample the data and add it to the arguments.
+		if not isinstance(data_b, (tuple, list)):
+			data_b = [data_b]
+
+		if not isinstance(data_a, (tuple, list)):
+			data_a = [data_a]
+
+		for db in reversed(data_b):
+			args.insert(0, self._resample(self._getcol(db, self.df_b), 1))
+
+		for da in reversed(data_a):
+			args.insert(0, self._resample(self._getcol(da, self.df_a), 0))
+
+		c = comp_func(*tuple(args), **kwargs)
+
+		# Store the result the comparison result
+		if store:
+			self._append(c, name=name)
+
+		return pandas.Series(c, index=self.pairs, name=name)
 
 	def exact(self, s1, s2, *args, **kwargs):
 		"""
-		exact(s1, s2, missing_value=0, disagreement_value=0, output='any', return_agreement_values=False)
+		exact(s1, s2, agree_value=False, disagree_value=0, missing_value=0, output='any')
 
 		Compare the record pairs exactly.
 
 		:param s1: Series or DataFrame to compare all fields. 
 		:param s2: Series or DataFrame to compare all fields. 
+		:param agree_value: If agree_value is True, each agreeing comparison returns the value instead of 1. Default False.		
+		:param disagree_value: The value when two records are not identical.
 		:param missing_value: The value for a comparison with a missing value. Default 0.
 		:param output: Default 'any'. This holds only for comparing dataframes.
-		:param return_agreement_values: If return_agreement_values is True, each agreeing comparison returns the value instead of 1. Default False.		
 
 		:return: A Series with comparison values.
 		:rtype: pandas.Series
 
 		"""
 
-		return self.compare(exact, s1, s2, *args, **kwargs)
+		return self.compare(_compare_exact, s1, s2, *args, **kwargs)
 
 	def numerical(self, s1, s2, *args, **kwargs):
 		"""
@@ -53,7 +126,7 @@ class Compare(object):
 
 		"""
 
-		return self.compare(window_numerical, s1, s2, *args, **kwargs)
+		return self.compare(_compare_numerical, s1, s2, *args, **kwargs)
 
 	def fuzzy(self, s1, s2, *args, **kwargs):
 		"""
@@ -74,11 +147,11 @@ class Compare(object):
 
 		"""
 
-		return self.compare(fuzzy, s1, s2, *args, **kwargs)
+		return self.compare(_compare_fuzzy, s1, s2, *args, **kwargs)
 
 	def geo(self, X1, Y1, X2, Y2, *args, **kwargs):
 		"""
-		geo(X1, Y1, X2, Y2, radius=20, disagreement_value = -1, missing_value=-1)
+		geo(X1, Y1, X2, Y2, radius=20, disagree_value = -1, missing_value=-1)
 
 		Compare geometric coordinates with a tolerance window.
 
@@ -87,7 +160,7 @@ class Compare(object):
 		:param X2: Series with X-coordinates
 		:param Y2: Series with Y-coordinates
 		:param missing_value: The value for a comparison with a missing value. Default -1.
-		:param disagreement_value: The value for a disagreeing comparison. Default -1.
+		:param disagree_value: The value for a disagreeing comparison. Default -1.
 
 		:param compare_method: levestein
 
@@ -95,54 +168,27 @@ class Compare(object):
 		:rtype: pandas.Series
 		"""
 
-		return self.compare(compare_geo, (X1, Y1), (X2, Y2), *args, **kwargs)
-
-	def compare(self, comp_func, data_a, data_b, *args, **kwargs):
-		"""Compare the records given. 
-
-		:param comp_func: A comparison function. This function can be a built-in function or a user defined comparison function.
-
-		:return: The DataFrame Compare.vectors
-		:rtype: standardise.DataFrame
-		"""
-
-		args = list(args)
-		name = kwargs.pop('name', None)
-		store = kwargs.pop('store', True)
-
-		print name
-
-		# Sample the data and add it to the arguments.
-		if not isinstance(data_b, (tuple, list)):
-			data_b = [data_b]
-
-		if not isinstance(data_a, (tuple, list)):
-			data_a = [data_a]
-
-		for db in reversed(data_b):
-			args.insert(0, self._resample(self._getcol(db, self.B), 1))
-
-		for da in reversed(data_a):
-			args.insert(0, self._resample(self._getcol(da, self.A), 0))
-
-		# print args
-		c = comp_func(*tuple(args), **kwargs)
-
-		# Add the comparison result
-		self._append(c, name=name, store=store)
-
-		return c
+		return self.compare(_compare_geo, (X1, Y1), (X2, Y2), *args, **kwargs)
 
 	def batchcompare(self, list_of_comp_funcs):
+		"""
+		This method will be used to speed up the comparison of record pairs in the future.
 
-		raise NotImplementedError()
+		"""
 
-	def _append(self, comp_vect, name=None, store=True):
+		raise NotImplementedError("This method will be used to increase the comparing speed.")
 
-		if store:
+	def _append(self, comp_vect, name=None):
+		"""
 
-			comp_vect.name = name
-			self.vectors[name] = comp_vect
+		Add the comparison result to the ``vector`` attribute.
+
+		"""
+
+		if name:
+			self.vectors[name] = comp_vect.values
+		else:
+			self.vectors[len(self.vectors)] = comp_vect
 
 	def _getcol(self, label_or_column, dataframe):
 		""" 
@@ -161,69 +207,28 @@ class Compare(object):
 
 		return data
 
-	def _compute_dimension(self, pairs):
-		'''
-		Internal function to compute the dimension of the comparison. Deduplication returns 1 and linking 2 dataframes returns 2.
-		'''
-
-		# duplication
-		if len(pairs.names) == 2 and pairs.names[0] == pairs.names[1]:
-			return 1
-		else:
-			return len(pairs.names)
-
 def _missing(*args):
 
-	return np.any(np.concatenate([np.array(pd.DataFrame(arg).isnull()) for arg in args], axis=1), axis=1)
+	return np.any(np.concatenate([np.array(pandas.DataFrame(arg).isnull()) for arg in args], axis=1), axis=1)
 
-def exact(s1, s2, missing_value=0, disagreement_value=0, output='any', return_agreement_values=False):
-	"""
-	Compare two series or dataframes exactly on all fields. 
-	"""
+def _compare_exact(s1, s2, agree_value=1, disagree_value=0, missing_value=0, output='any'):
 
-	df1 = pd.DataFrame(s1)
-	df2 = pd.DataFrame(s2)
-
-	# Only when one of the input variables is a DataFrame
-	if len(list(df1)) > 1 or len(list(df2)) > 1:
-
-		compare = pd.DataFrame([(df1[col1] == df2[col2]) for col2 in list(df2) for col1 in list(df1)]).T
-
-		# Any of the agreeing comparisons
-		if output == 'any':
-			compare = compare.any(axis=1)
-			compare = compare.astype(int)
-
-		# Max of the comparisons
-		elif output == 'max':
-			compare = compare.astype(int)
-			compare = compare.max(axis=1)
-
-		# Sum of the comparison vectors
-		elif output == 'sum':
-			compare = compare.sum(axis=1)
-
-		# Unknown method
-		else:
-			raise ValueError('Unknown output method.')
+	if agree_value == 'value':
+		compare = s1.copy()
+		compare[(s1.values != s2.values)] = disagree_value
 
 	else:
+		compare = np.where(s1.values == s2.values, agree_value, disagree_value)
+		
+	compare = pandas.Series(compare, index=s1.index)
 
-		if not return_agreement_values:
-			compare = (s1 == s2)
-			compare = compare.astype(int)
-			compare.loc[(s1 != s2)] = disagreement_value 
+	# Only when disagree value is not identical with the missing value
+	if disagree_value != missing_value:
+		compare[_missing(s1, s2)] = missing_value
 
-		else:
-			compare = s1.copy()
-			compare.loc[(s1 != s2)] = disagreement_value
+	return compare
 
-	# Only for missing values
-	compare[_missing(df1, df2)] = missing_value
-
-	return pd.Series(compare)
-
-def window_numerical(s1, s2, window, missing_value=0):
+def _compare_numerical(s1, s2, window, missing_value=0):
 
 	if isinstance(window, (list, tuple)):
 		compare = (((s1-s2) <= window[1]) & ((s1-s2) >= window[0])).astype(int)
@@ -234,42 +239,48 @@ def window_numerical(s1, s2, window, missing_value=0):
 
 	return compare
 
-def compare_geo(X1, Y1, X2, Y2, radius=None, missing_value=9):
+def _compare_geo(X1, Y1, X2, Y2, radius=None, missing_value=np.nan):
 
-	distance = np.sqrt(np.power(X1-X2,2)+np.power(Y1-Y2,2))
+	try:
+		from scipy.spatial import distance
+	except ImportError:
+		raise ImportError('Could not import scipy.spatial')
 
-	comp = (distance <= radius).astype(int)
+	coord_1 = np.append([X1],[Y1], axis=0).T
+	coord_2 = np.append([X2],[Y2], axis=0).T
 
-	comp[_missing(X1, X2)] = missing_value
-	comp[_missing(Y1, Y2)] = missing_value
+	d = distance.cdist(coord_1, coord_2, 'cityblock')
 
-	return comp 
+	if not np.isnan(missing_value):
+		d[_missing(d)] = missing_value
 
-def fuzzy(s1,s2, method='levenshtein', threshold=None, missing_value=0):
+	return d 
+
+def _compare_fuzzy(s1,s2, method='levenshtein', threshold=None, missing_value=0):
 
 	try:
 		import jellyfish
 	except ImportError:
-		print "Install jellyfish to use approximate string comparison."
+		raise ImportError("Install 'jellyfish' to use approximate string comparison.")
 
-	series = pd.concat([s1, s2], axis=1)
+	series = pandas.concat([s1, s2], axis=1)
 
 	if method == 'jaro':
-		approx = series.apply(lambda x: jellyfish.jaro_distance(x[0], x[1]) if pd.notnull(x[0]) and pd.notnull(x[1]) else np.nan, axis=1)
+		approx = series.apply(lambda x: jellyfish.jaro_distance(x[0], x[1]) if pandas.notnull(x[0]) and pandas.notnull(x[1]) else np.nan, axis=1)
 	
 	elif method == 'jarowinkler':
-		approx = series.apply(lambda x: jellyfish.jaro_winkler(x[0], x[1]) if pd.notnull(x[0]) and pd.notnull(x[1]) else np.nan, axis=1)
+		approx = series.apply(lambda x: jellyfish.jaro_winkler(x[0], x[1]) if pandas.notnull(x[0]) and pandas.notnull(x[1]) else np.nan, axis=1)
 	
 	elif method == 'levenshtein':
-		approx = series.apply(lambda x: jellyfish.levenshtein_distance(x[0], x[1])/np.max([len(x[0]),len(x[1])]) if pd.notnull(x[0]) and pd.notnull(x[1]) else np.nan, axis=1)
+		approx = series.apply(lambda x: jellyfish.levenshtein_distance(x[0], x[1])/np.max([len(x[0]),len(x[1])]) if pandas.notnull(x[0]) and pandas.notnull(x[1]) else np.nan, axis=1)
 		approx = 1 - approx
 
 	elif method == 'damerau_levenshtein':
-		approx = series.apply(lambda x: jellyfish.damerau_levenshtein_distance(x[0], x[1])/np.max([len(x[0]),len(x[1])]) if pd.notnull(x[0]) and pd.notnull(x[1]) else np.nan, axis=1)
+		approx = series.apply(lambda x: jellyfish.damerau_levenshtein_distance(x[0], x[1])/np.max([len(x[0]),len(x[1])]) if pandas.notnull(x[0]) and pandas.notnull(x[1]) else np.nan, axis=1)
 		approx = 1 - approx
 
 	else:
-		raise ValueError('The method %s is not found.' % method)
+		raise ValueError('Algorithm {} not found.'.format(method))
 
 	if threshold is not None:
 		comp = (approx >= threshold).astype(int)
@@ -278,44 +289,5 @@ def fuzzy(s1,s2, method='levenshtein', threshold=None, missing_value=0):
 
 	# Only for missing values
 	comp[_missing(s1, s2)] = missing_value
-
-	return comp
-
-def window(s1, s2, window, missing_value=0, disagreement_value=0, sim_func=None):
-
-	diff = s2-s1
-
-	w = window if isinstance(window, (list, tuple)) else (window,window)
-
-	if sim_func == None:
-		sim = diff[(diff <= window[1]) & (diff >= window[0])]
-
-	elif sim_func == 'linear':
-		pass
-
-	else:
-		compare = (((s1-s2) <= window) & ((s1-s2) >= window)).astype(int)
-
-	compare[_missing(s1, s2)] = missing_value 
-
-	return compare
-
-def bin_compare_geq(cat1, cat2, missing_value=0):
-
-	# compare
-	comp = (cat1 >= cat2).astype(int)
-	comp.ix[_missing(cat1, cat2)] = missing_value 
-
-	return comp
-
-def compare_levels(s1, s2, freq, split=np.array([1, 5, 10, 20, 50, 100, np.inf])):
-
-	comp = freq.copy()
-
-	comp.fillna(0, inplace=True)
-	comp.loc[(s1 != s2)] = 0
-
-	for sp in range(0, len(split)-1):
-		comp.loc[(comp < split[sp+1]) & (comp >= split[sp])] = split[sp]
 
 	return comp
