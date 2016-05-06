14a23b68a8503abb71f131b52c46f83c0adc1d71
jonathandebruin <jonathandebruinhome@gmail.com>
2016-05-06 17:09:30 +0200

    Better test data and and numerical test

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -6,33 +6,33 @@ import numpy as np
 import pandas as pd
 
 TEST_DATA_1 = pd.DataFrame([
-    [u'bob', u'smith', 10, u'new york'],
-    [np.nan, u'smith', 10, u'new york'],
-    [u'bob2', u'smith', 10, u'new york'],
-    [u'bob2', u'smith', 10, u'new york'],
-    [u'bob', u'smith', np.nan, u'new york']
+    [u'Donell', u'Gerlach', 20, u'New York'],
+    [np.nan, u'Smit', 17, u'Boston'],
+    [u'Kalie', u'Flatley', 33, u'Boston'],
+    [u'Kittie', u'Schuster', 27, np.nan],
+    [np.nan, np.nan, np.nan, u'South Devyn']
     ],
-    columns=['name', 'lastname', 'age', 'place'],
-    index=pd.Index([1,2,3,4,5], name='index_df1')
-    )
+    columns=['name', 'lastname', 'age', 'place'])
+
+TEST_DATA_1.index.name = 'index_df1'
 
 TEST_DATA_2 = pd.DataFrame([
-    [u'bob', u'smith', 10, u'new york'],
-    [np.nan, u'smith', 10, u'new york'],
-    [u'bob2', u'smith', 10, u'new york'],
-    [u'bob2', u'smith', np.nan, u'new york'],
-    [u'bob', u'smith', np.nan, u'new york']
+    [u'Donel', u'Gerleach', 20, u'New York'],
+    [np.nan, u'Smith', 17, u'Boston'],
+    [u'Kaly', u'Flatley', 33, u'Boston'],
+    [u'Kittie', np.nan, 20, np.nan],
+    [u'Bob', u'Armstrong', 70, u'Lake Gavinmouth']
     ],
-    columns=['name', 'lastname', 'age', 'place'],
-    index=pd.Index([1,2,3,4,5], name='index_df2')
-    )
+    columns=['name', 'lastname', 'age', 'place'])
+
+TEST_DATA_2.index.name = 'index_df2'
 
 TEST_INDEX_LINKING = pd.MultiIndex.from_arrays(
-    [[1,1,2,3,4,5], [2,4,3,5,5,1]], 
+    [np.arange(len(TEST_DATA_1)), np.arange(len(TEST_DATA_2))], 
     names=[TEST_DATA_1.index.name, TEST_DATA_2.index.name])
 
 TEST_INDEX_DEDUP = pd.MultiIndex.from_arrays(
-    [[1,1,2,3,4,5], [2,4,3,5,5,1]], 
+    [np.arange(len(TEST_DATA_1)), np.arange(len(TEST_DATA_1))], 
     names=[TEST_DATA_1.index.name, TEST_DATA_1.index.name])
 
 
@@ -44,7 +44,7 @@ class TestCompare(unittest.TestCase):
 
         # Missing values
         result = comp.exact('name', 'name', name='y_name')
-        expected = pd.Series([0,0,0,0,0,1], index=TEST_INDEX_LINKING, name='y_name')
+        expected = pd.Series([0,0,0,1,0], index=TEST_INDEX_LINKING, name='y_name')
 
         pdt.assert_series_equal(result, expected)
 
@@ -54,19 +54,19 @@ class TestCompare(unittest.TestCase):
 
         # Missing values as 0
         result = comp.exact('name', 'name', missing_value=0, name='y_name')
-        expected = pd.Series([0,0,0,0,0,1], index=TEST_INDEX_LINKING, name='y_name')
+        expected = pd.Series([0,0,0,1,0], index=TEST_INDEX_LINKING, name='y_name')
 
         pdt.assert_series_equal(result, expected)
 
         # Missing values as np.nan
         result = comp.exact('name', 'name', missing_value=np.nan, name='y_name')
-        expected = pd.Series([np.nan,0,np.nan,0,0,1], index=TEST_INDEX_LINKING, name='y_name')
+        expected = pd.Series([0,np.nan,0,1,np.nan], index=TEST_INDEX_LINKING, name='y_name')
 
         pdt.assert_series_equal(result, expected)
 
         # Missing values as np.nan
         result = comp.exact('name', 'name', missing_value=9, name='y_name')
-        expected = pd.Series([9,0,9,0,0,1], index=TEST_INDEX_LINKING, name='y_name')
+        expected = pd.Series([0,9,0,1,9], index=TEST_INDEX_LINKING, name='y_name')
 
         pdt.assert_series_equal(result, expected)
 
@@ -76,7 +76,7 @@ class TestCompare(unittest.TestCase):
 
         # Missing values 0 and disagreement as 2
         result = comp.exact('name', 'name', disagree_value=2, missing_value=0, name='y_name')
-        expected = pd.Series([0,2,0,2,2,1], index=TEST_INDEX_LINKING, name='y_name')
+        expected = pd.Series([2,0,2,1,0], index=TEST_INDEX_LINKING, name='y_name')
 
         pdt.assert_series_equal(result, expected)
 
@@ -86,10 +86,21 @@ class TestCompare(unittest.TestCase):
 
         # Missing values
         result = comp.exact('name', 'name', name='y_name')
-        expected = pd.Series([0,0,0,0,0,1], index=TEST_INDEX_DEDUP, name='y_name')
+        expected = pd.Series([1,0,1,1,0], index=TEST_INDEX_DEDUP, name='y_name')
 
         pdt.assert_series_equal(result, expected)
-       
+
+    def test_numerical(self):
+
+        comp = recordlinkage.Compare(TEST_INDEX_LINKING, TEST_DATA_1, TEST_DATA_2)
+
+        # Missing values
+        result = comp.numerical('age', 'age', 2)
+        expected = pd.Series([1,1,1,0,0], index=TEST_INDEX_LINKING, name='y_name')
+
+        pdt.assert_series_equal(result, expected)
+
+
     def test_fuzzy_does_not_exist(self):
         
         comp = recordlinkage.Compare(TEST_INDEX_DEDUP, TEST_DATA_1, TEST_DATA_1)
@@ -98,7 +109,7 @@ class TestCompare(unittest.TestCase):
 
     def test_fuzzy_same_labels(self):
         
-        comp = recordlinkage.Compare(TEST_INDEX_DEDUP, TEST_DATA_1, TEST_DATA_1)
+        comp = recordlinkage.Compare(TEST_INDEX_DEDUP, TEST_DATA_1, TEST_DATA_2)
 
         for alg in ['jaro', 'jaro_winkler', 'dameraulevenshtein', 'levenshtein', 'q_gram', 'cosine']:
 
@@ -115,14 +126,14 @@ class TestCompare(unittest.TestCase):
 
     def test_fuzzy_different_labels(self):
         
-        comp = recordlinkage.Compare(TEST_INDEX_DEDUP, TEST_DATA_1, TEST_DATA_1)
+        comp = recordlinkage.Compare(TEST_INDEX_DEDUP, TEST_DATA_1, TEST_DATA_2)
 
         for alg in ['jaro', 'jaro_winkler', 'dameraulevenshtein', 'levenshtein', 'q_gram', 'cosine']:
 
             print (alg)
 
             # Missing values
-            result = comp.fuzzy('name', 'lastname', method=alg, missing_value=np.nan) # Change in future (should work without method)
+            result = comp.fuzzy('name', 'name', method=alg, missing_value=np.nan) # Change in future (should work without method)
             
             print (result)
 
@@ -130,5 +141,8 @@ class TestCompare(unittest.TestCase):
             self.assertTrue((result[result.notnull()] >= 0).all())
             self.assertTrue((result[result.notnull()] <= 1).all())
 
+            # Debug trick
+            # if alg == 'q_gram':
+            #     rr
 
 
