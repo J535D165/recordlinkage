477e200d36de1ec83146fd0117745997ae3d7357
jonathandebruin <jonathandebruinhome@gmail.com>
2016-05-15 23:21:22 +0200

    Update clean documentation

--- recordlinkage/standardise/cleaning.py
+++ recordlinkage/standardise/cleaning.py
@@ -12,12 +12,20 @@ def clean(s, lower=True, replace_by_none='[^ \-\_A-Za-z0-9]+', replace_by_whites
     """
     clean(lower=True, replace_by_none='[^ \-\_A-Za-z0-9]+', replace_by_whitespace='[\-\_]', remove_brackets=True)
     
-    Remove special tokens from a series of strings. 
+    Clean a pandas Series with strings. Remove unwanted tokens and additional
+    whitespace.
 
+    :param s: A pandas.Series to clean.
     :param lower: Convert the strings in lowercase characters.
     :param replace_by_none: A regular expression that is replaced by ''.
     :param replace_by_whitespace: A regular expression that is replaced by a whitespace.
     :param remove_brackets: Remove all content between brackets and the brackets themselves. 
+
+    :type s: pandas.Series
+    :type lower: boolean
+    :type replace_by_none: string
+    :type replace_by_whitespace: string
+    :type remove_brackets: boolean
 
     :return: A cleaned Series of strings.
     :rtype: pandas.Series
