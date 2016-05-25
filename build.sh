35f38f12095f765876231847d28fc7f9c097ad80
jonathandebruin <jonathandebruinhome@gmail.com>
2016-05-25 10:10:23 +0200

    Update build with virtual environments

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
 # Run in terminal with: sh build.sh
 
+virtualenv -p /usr/bin/python2.7 env/python2
+virtualenv -p /usr/bin/python3.5 env/python3
+
+source env/python2/bin/activate
+pip install -r requirements.txt
+
+nosetests
+
+deactivate
+rm -rf env/python2
+
+source env/python3/bin/activate
+pip install -r requirements.txt
+
+nosetests
+
+deactivate
+rm -rf env/python3
+
 # Create the documentation.
 pandoc --from=markdown_github --to=rst --output=docs/README.rst README.md
 
@@ -10,8 +29,9 @@ make clean
 make html
 make latexpdf
 
-# Make the package installable. 
 cd ..
+
+# Make the package installable. 
 python setup.py sdist
 python setup.py build
 python setup.py install
