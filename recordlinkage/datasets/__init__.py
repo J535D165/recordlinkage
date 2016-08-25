2d23649e41dac25d521134a981532641a45359b2
Jonathan <jonathandebruinhome@gmail.com>
2016-08-25 23:20:02 +0200

    Fix broken documentation

--- recordlinkage/datasets/__init__.py
+++ recordlinkage/datasets/__init__.py
@@ -181,14 +181,11 @@ def load_febrl4():
 	with a dataset generator and four datasets generated with the generator.
 	This functions returns the fourth Febrl dataset as a pandas DataFrame.
 
-		*"Generated as one data set with 10000 records (5000 originals and
-		5000 duplicates, with one duplicate per original), the originals
-		have been split from the duplicates, into
-
-		dataset4a.csv (containing the 5000 original records)
-		dataset4b.csv (containing the 5000 duplicate records)
-
-		These two data sets can be used for testing linkage procedures."*
+	    *"Generated as one data set with 10000 records (5000 originals and
+		5000 duplicates, with one duplicate per original), the originals have been
+		split from the duplicates, into dataset4a.csv (containing the 5000 original
+		records) and dataset4b.csv (containing the 5000 duplicate records) These two
+		data sets can be used for testing linkage procedures."*
 
 	:return: A pandas DataFrame with Febrl dataset4a.csv and a pandas
 				DataFrame with Febrl dataset4b.csv. 
