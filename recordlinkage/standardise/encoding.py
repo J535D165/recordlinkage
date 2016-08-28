10f30b36e2aeb01f8a69991fceffe639279b28f0
Jonathan <jonathandebruinhome@gmail.com>
2016-08-28 15:51:53 +0200

    Add a new gender prediction tool and documentation

--- recordlinkage/standardise/encoding.py
+++ recordlinkage/standardise/encoding.py
@@ -2,9 +2,11 @@ from __future__ import division
 # from __future__ import absolute_import
 
 import logging
+import warnings
+import os
 
 import numpy as np
-import pandas as pd
+import pandas
 
 import itertools
 
@@ -32,13 +34,136 @@ def phonetic(s, method):
     s = clean(s, replace_by_none='[^\-\_A-Za-z0-9]+')
  
     if method == 'soundex':
-        return s.str.upper().apply(lambda x: jellyfish.soundex(x) if pd.notnull(x) else np.nan)
+        return s.str.upper().apply(lambda x: jellyfish.soundex(x) if pandas.notnull(x) else np.nan)
 
     elif method == 'nysiis':
-        return s.str.upper().apply(lambda x: jellyfish.nysiis(x) if pd.notnull(x) else np.nan)
+        return s.str.upper().apply(lambda x: jellyfish.nysiis(x) if pandas.notnull(x) else np.nan)
 
     else:
         raise Exception("Phonetic encoding method not found")
+
+def gender(names, years=None, method="ssa", countries=None, labels=("male", "female"), q=0.1):
+    """
+    Predict the gender based on the first name. This tool is based on https://github.com/ropensci/gender and uses their data. 
+
+    :param names: The given names.
+    :param years: The birth year of the first names whose gender is to be predicted. 
+            This argument can be either a single year, a range of years in the form
+            [1880, 1900]. If no value is specified, then for the ``ssa``
+            method it will use the period 1932 to 2012. 
+    :param method: This value determines the data set that is used to predict the
+            gender of the name. The ``ssa`` method looks up names based from the
+            U.S. Social Security Administration baby name data.
+    :param countries: The country. 
+    :param labels: The label given for male and female prediction. Default: ("male", "female").
+    :param sex_ratio: The ratio of males to females in the newborn population (`wikipedia 
+            <https://en.wikipedia.org/wiki/Sex_ratio>`_). Default 1.07
+    :param q: The maximum error probability for a name. 
+
+    :type names: pandas.Series
+    :type years: str, int, (list, tuple), (pandas.Series, numpy.array)
+    :type method: "ssa"
+    :type countries: "United States"
+    :type labels: 2-tuple or list
+    :type q: float
+    :type sex_ratio: float
+
+    :return: The gender predictions for each row.
+    :rtype: pandas.Series
+
+    """
+
+    if method == "ssa" and (countries== "United States" or countries is None):
+        return gender_ssa(names, years, labels=labels, q=q)
+    else:
+        raise NotImplementedError("This chosen method {} is not known.".format(method))
+
+def gender_ssa(names, years=None, labels=("male", "female"), q=0.49, sex_ratio=1.07):
+
+    filepath = os.path.join(os.path.dirname(__file__), 'data', 'ssa_national.zip')
+    fileurl = ""
+
+    if os.path.exists(filepath):
+        ssa_national = pandas.read_csv(filepath, compression="zip")
+    else: 
+        try:
+            ssa_national = pandas.read_csv(fileurl, compression="zip")
+        except Exception:
+            raise Exception("Unable the load the dataset.")
+    
+    ssa_min_year = int(ssa_national["year"].min())
+    ssa_max_year = int(ssa_national["year"].max())
+        
+    if isinstance(years, (pandas.Series, np.ndarray)): # pandas.Series
+        
+        if len(names) != len(years):
+            raise ValueError("Length of names and years are not the same.")
+        
+        years = [int(years.min()), int(years.max())]
+        
+        if (years[1] > ssa_max_year) or (years[0] < ssa_min_year):
+            warnings.warn("years outside the range {} - {}. Using truncated average.".format(
+                    ssa_min_year, ssa_max_year))
+        else:
+            s_years = pandas.Series(years)
+            
+    elif isinstance(years, (list, tuple)):
+        years = [min(years), max(years)]   
+        
+    if years is not None: # None or empty list
+        years = [1930, ssa_max_year]
+        
+    elif not isinstance(years, (list, tuple)):
+        years = [years, years]   
+        
+    else:
+        raise ValueError("years is not a valid type.")
+            
+    if (years[1] > ssa_max_year) or (years[0] < ssa_min_year):
+        raise ValueError("Use years in the range {} - {}".format(ssa_min_year, ssa_max_year))
+            
+    # Make subset of relevant years
+    ssa_national_subset = ssa_national[
+        (ssa_national["year"] >= years[0]) & \
+        (ssa_national["year"] <= years[1])
+    ]
+    
+    # Correct the skew. Different than in https://github.com/ropensci/gender
+    if 's_years' not in locals():
+        ssa_national_grouped = ssa_national_subset.groupby('name')[['female', 'male']].sum()
+    else:
+        ssa_national_grouped = ssa_national_subset.copy().set_index(['name', 'year'])
+        
+    # compute sex ratio https://en.wikipedia.org/wiki/Sex_ratio with 
+    if not sex_ratio:
+        P_male = ssa_national_grouped['male']/(ssa_national_grouped['male'] + ssa_national_grouped['female'])
+        P_female = ssa_national_grouped['female']/(ssa_national_grouped['male'] + ssa_national_grouped['female'])
+        sex_ratio = P_male/P_female
+
+    P_name_given_male = ssa_national_grouped['male']/ssa_national_grouped['male'].sum()
+    P_name_given_female = ssa_national_grouped['female']/ssa_national_grouped['female'].sum()
+
+    P_male_given_name = (P_name_given_male*sex_ratio)/((P_name_given_male*sex_ratio) + P_name_given_female)
+
+    # Proportion
+    if q <= 0 or q >= 0.5:
+        raise ValueError("q must be a value between 0 and 0.5. ")
+            
+    ssa_national_grouped['prediction'] = np.nan
+    ssa_national_grouped.loc[P_male_given_name >= 1-q , "prediction"] = labels[0]
+    ssa_national_grouped.loc[P_male_given_name <= q , "prediction"] = labels[1]
+    
+    # 
+    if 's_years' in locals():
+        return pandas.Series(
+            ssa_national_grouped.loc[pandas.MultiIndex.from_arrays([names.fillna(""), s_years]), "prediction"].values, 
+            index=names.index.values
+        )        
+    else:
+        return pandas.Series(
+            ssa_national_grouped.loc[names.fillna(""), "prediction"].values,  
+            index=names.index
+        )
 
 def similar_values(s, threshold=0.8):
     """
@@ -71,9 +196,9 @@ def similar_values(s, threshold=0.8):
     for pair in replace_tuples: 
 
         if (sum(self == pair[0]) > sum(self == pair[1])):
-            self = pd.Series(self.str.replace(pair[1], pair[0]))
+            self = pandas.Series(self.str.replace(pair[1], pair[0]))
         else:
-            self = pd.Series(self.str.replace(pair[0], pair[1]))
+            self = pandas.Series(self.str.replace(pair[0], pair[1]))
 
-    return pd.Series(string)
+    return pandas.Series(string)
 
