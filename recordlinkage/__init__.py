da3ac6f81f04a43e94609f56d831ac5b4215a8ff
Jonathan <jonathandebruinhome@gmail.com>
2016-09-30 23:44:22 +0200

    Restructure imports order

--- recordlinkage/__init__.py
+++ recordlinkage/__init__.py
@@ -1,14 +1,11 @@
 
+from .indexing import *
+from .comparing import *
+from .classifiers import *
+from .measures import *
+
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
 __all__ = ['standardise', 'datasets']
-
-# # Import important modules
-# from recordlinkage.standardise import *
-
-from .indexing import *
-from .comparing import *
-from .classifiers import *
-from .measures import *
