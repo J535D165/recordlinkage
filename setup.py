a983bff00d80373b25467b5d901d34529a94df6f
Jonathan <jonathandebruinhome@gmail.com>
2016-10-12 14:42:04 +0200

    Update license --> GPL-3.0

--- setup.py
+++ setup.py
@@ -1,31 +1,34 @@
 import os
 
-from setuptools import setup, find_packages
+from setuptools import setup
 import versioneer
 
+
 def read(fname):
-	return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 
 setup(
-	name="recordlinkage",
-	version=versioneer.get_version(),
-	cmdclass=versioneer.get_cmdclass(),
-  	author="Jonathan de Bruin",
-  	author_email="jonathandebruinhome@gmail.com",
+    name="recordlinkage",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    author="Jonathan de Bruin",
+    author_email="jonathandebruinhome@gmail.com",
 
-	platforms="any",
+    platforms="any",
 
-	# Description
-	description="A record linkage toolkit for linking and deduplication",
-	long_description=read('README.rst'),
+    # Description
+    description="A record linkage toolkit for linking and deduplication",
+    long_description=read('README.rst'),
 
-	# Documentation
-	url="http://recordlinkage.readthedocs.io/",
+    # Documentation
+    url="http://recordlinkage.readthedocs.io/",
 
-	install_requires=["numpy", "pandas", "scipy", "sklearn"],
-	packages=['recordlinkage', 'recordlinkage.datasets', 'recordlinkage.standardise'],
-	include_package_data=True,
-	package_dir={'recordlinkage': 'recordlinkage'},
-	package_data={'recordlinkage': ['datasets/*/*.csv']},
-	license='GNU'
+    install_requires=["numpy", "pandas", "scipy", "sklearn"],
+    packages=['recordlinkage', 'recordlinkage.datasets',
+              'recordlinkage.standardise'],
+    include_package_data=True,
+    package_dir={'recordlinkage': 'recordlinkage'},
+    package_data={'recordlinkage': ['datasets/*/*.csv']},
+    license='GPL-3.0'
 )
