e0f376270ea4efe1f9b908f0ffcbbab4d4da171a
Jonathan <jonathandebruinhome@gmail.com>
2016-10-30 16:16:31 +0100

    Restructure tests and add return_type tests

--- tests/test_classify.py
+++ tests/test_classify.py
@@ -1,18 +1,75 @@
 import unittest
 
+import pandas
+import numpy
 
 import recordlinkage
-from recordlinkage.datasets import load_krebsregister
 
 
-class TestClassify(unittest.TestCase):
+# nosetests tests/test_classify.py:TestClassifyData --with-coverage --cover-package=recordlinkage
+class TestClassifyData(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
 
-        self.y, self.match_index = load_krebsregister(1)
-        self.y.fillna(0, inplace=True)
+        N = 10000
+        seed = 101
+        seed_gold = 1234
 
+        # set random state
+        numpy.random.seed(seed)
+
+        self.y = pandas.DataFrame(
+            numpy.random.random((N, 7)),
+            index=pandas.MultiIndex.from_arrays(
+                [numpy.arange(0, N), numpy.arange(0, N)]),
+            columns=[
+                'name', 'second_name', 'surname',
+                'age', 'street', 'state', 'zipcode'
+            ]
+        )
+
+        self.matches_array = numpy.random.random_integers(0, 1, N)
+        self.matches_series = pandas.Series(self.matches_array)
+        self.matches_index = self.matches_series[self.matches_series == 1].index
+
+        self.y_train = self.y.sample(1000, replace=False, random_state=seed_gold)
+        self.y_train_index = self.y_train.index
+
+
+# nosetests tests/test_classify.py:TestClassifyAPI --with-coverage --cover-package=recordlinkage
+class TestClassifyAPI(TestClassifyData):
+
+    def test_return_result_options(self):
+
+        cl = recordlinkage.Classifier()
+
+        prediction_default = cl._return_result(
+            self.matches_array, comparison_vectors=self.y)
+        self.assertTrue(isinstance(prediction_default, pandas.MultiIndex))
+
+        prediction_multiindex = cl._return_result(
+            self.matches_array, return_type='index', comparison_vectors=self.y)
+        self.assertTrue(isinstance(prediction_multiindex, pandas.MultiIndex))
+
+        prediction_ndarray = cl._return_result(
+            self.matches_array, return_type='array', comparison_vectors=self.y)
+        self.assertTrue(isinstance(prediction_ndarray, numpy.ndarray))
+
+        prediction_series = cl._return_result(
+            self.matches_array, return_type='series', comparison_vectors=self.y)
+        self.assertTrue(isinstance(prediction_series, pandas.Series))
+
+        with self.assertRaises(ValueError):
+            cl._return_result(
+                self.matches_array,
+                return_type='unknown_return_type',
+                comparison_vectors=self.y
+            )
+
+
+# nosetests tests/test_classify.py:TestClassifyAlgorithms --with-coverage --cover-package=recordlinkage
+class TestClassifyAlgorithms(TestClassifyData):
 
     def test_kmeans(self):
 
