3cb84a5e0d244f45d279ff5d7b57ce5c77e0f1d7
Jonathan <jonathandebruinhome@gmail.com>
2017-02-04 13:24:29 +0100

    TESTS: Add tests to test the generation of binary vectors

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -2,11 +2,15 @@ import unittest
 
 import pandas
 
-from recordlinkage.datasets import load_febrl1, \
-    load_febrl2, load_febrl3, load_febrl4, load_krebsregister
+from recordlinkage.datasets import (load_febrl1,
+                                    load_febrl2,
+                                    load_febrl3,
+                                    load_febrl4,
+                                    load_krebsregister,
+                                    binary_vectors)
 
-# nosetests tests/test_datasets.py:TestDatasets
-class TestDatasets(unittest.TestCase):
+# nosetests tests/test_datasets.py:TestExternalDatasets
+class TestExternalDatasets(unittest.TestCase):
 
     def test_datasets_existance(self):
 
@@ -50,3 +54,26 @@ class TestDatasets(unittest.TestCase):
 
         # missing values
         krebs_data_block10, krebs_matches = load_krebsregister(10, shuffle=False)
+
+
+# nosetests tests/test_datasets.py:TestGeneratedDatasets
+class TestGeneratedDatasets(unittest.TestCase):
+
+    def test_random_comparison_vectors(self):
+        # Test the generation of a random dataset
+
+        n_record_pairs = 10000
+        n_matches = 500
+
+        df = binary_vectors(n_record_pairs, n_matches,
+                            m=[0.8] * 8, u=[0.2] * 8,
+                            random_state=535)
+
+        # Check the result is a DataFrame with MultiIndex
+        self.assertIsInstance(df, pandas.DataFrame)
+        self.assertIsInstance(df.index, pandas.MultiIndex)
+
+        # Test the length of the dataframe
+        self.assertEqual(len(df), n_record_pairs)
+
+
