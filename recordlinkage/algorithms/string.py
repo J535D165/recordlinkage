2fcfbadbeb349ac152b1e043b4934fb8b312c5b2
Joel Becker <mail@joelbecker.ca>
2017-07-14 15:14:02 -0400

    Implemented longest common substring algorithm.

--- recordlinkage/algorithms/string.py
+++ recordlinkage/algorithms/string.py
@@ -147,3 +147,110 @@ def cosine_similarity(s1, s2, include_wb=True, ngram=(2, 2)):
         return np.divide(ab, np.multiply(a, b)).A1
 
     return _metric_sparse_cosine(vec_fit[:len(s1)], vec_fit[len(s1):])
+
+
+def longest_common_substring_similarity(s1, s2, norm='dice', min_len=2):
+    """
+    An implementation of the lognest common substring similarity algorith
+    described in Christen, Peter (2012).
+
+    :param pd.Series s1: Comparison data.
+    :param pd.Series s2: Comparison data.
+    :param str norm: The normalization applied to the raw length computed by the lcs algorithm.
+    :param int min_len: The minimum length of substring to be considered.
+    :return: A pd.Series of normalized similarity values.
+    """
+
+    if len(s1) != len(s2):
+        raise ValueError('Arrays or Series have to be same length.')
+
+    if len(s1) == len(s2) == 0:
+        return []
+
+    conc = pandas.concat([s1, s2], axis=1, ignore_index=True)
+
+    def lcs_iteration(x):
+        """
+        A helper function implementation of a single iteration longest common substring algorithm,
+        adapted from
+        https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Longest_common_substring.
+        but oriented towards the iterative approach described by Christen, Peter (2012).
+        """
+
+        str1 = x[0]
+        str2 = x[1]
+
+        if str1 is np.nan or str2 is np.nan or min(len(str1), len(str2)) < min_len:
+            longest = 0
+            new_str1 = None
+            new_str2 = None
+        else:
+            # Creating a matrix of 0s
+            m = [[0] * (1 + len(str2)) for _ in range(1 + len(str1))]
+
+            longest = 0
+            x_longest = 0
+            y_longest = 0
+
+            for x in range(1, 1 + len(str1)):
+                for y in range(1, 1 + len(str2)):
+                    # Check if the chars match
+                    if str1[x - 1] == str2[y - 1]:
+                        # add 1 to the diagnol
+                        m[x][y] = m[x - 1][y - 1] + 1
+                        if m[x][y] > longest:
+                            longest = m[x][y]
+                            x_longest = x
+                            y_longest = y
+                    else:
+                        m[x][y] = 0
+
+            new_str1 = str1[0:x_longest-longest]+str1[x_longest:]
+            new_str2 = str2[0:y_longest-longest]+str2[y_longest:]
+
+        return (new_str1, new_str2), longest
+
+    def lcs_apply(x):
+
+        if pandas.isnull(x[0]) or pandas.isnull(x[1]):
+            return np.nan
+
+        # Compute lcs value with first ordering.
+        lcs_acc_1 = 0
+        new_x_1 = (x[0], x[1])
+
+        while True:
+            iter_x, iter_lcs = lcs_iteration(new_x_1)
+            if iter_lcs < min_len:
+                break
+            else:
+                new_x_1 = iter_x
+                lcs_acc_1 = lcs_acc_1 + iter_lcs
+
+        # Compute lcs value with second ordering.
+        lcs_acc_2 = 0
+        new_x_2 = (x[1], x[0])
+
+        while True:
+            iter_x, iter_lcs = lcs_iteration(new_x_2)
+            if iter_lcs < min_len:
+                break
+            else:
+                new_x_2 = iter_x
+                lcs_acc_2 = lcs_acc_2 + iter_lcs
+
+        def normalize_lcs(lcs_value):
+            if norm == 'overlap':
+                return lcs_value / min(len(x[0]), len(x[1]))
+            elif norm == 'jaccard':
+                return lcs_value / (len(x[0])+len(x[1])-abs(lcs_value))
+            elif norm == 'dice':
+                return lcs_value*2 / (len(x[0])+len(x[1]))
+            else:
+                warnings.warn('Unrecognized longest common substring normalization. Defaulting to "dice" method.')
+                return lcs_value*2 / (len(x[0])+len(x[1]))
+
+        # Average the two orderings.
+        return (normalize_lcs(lcs_acc_1)+normalize_lcs(lcs_acc_2)) / 2
+
+    return conc.apply(lcs_apply, axis=1)
