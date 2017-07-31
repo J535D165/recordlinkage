eba81e7b044ae37ad51db86bebab8414f05ff55d
Joel Becker <mail@joelbecker.ca>
2017-07-31 14:48:59 -0400

    Moved CRFs to rl/algorithms, realized a few CR strategies in fuse.py.

---
+++ recordlinkage/algorithms/conflict_resolution.py
@@ -0,0 +1,235 @@
+from __future__ import division
+from __future__ import unicode_literals
+
+import random
+import collections
+import statistics
+import numpy as np
+
+
+# Conflict Resolution Decorator
+def conflict_resolution_function(metadata=None, param=None):
+    """
+    Used to assert high-level requirements in the
+    conflict resolution process.
+
+    :param bool metadata: True/False
+    :param bool param: True/False
+    :return: function
+    """
+
+    def decorate(func):
+        func.metadata = metadata
+        func.param = param
+        return func
+
+    return decorate
+
+
+# Conflict Resolution Functions
+
+@conflict_resolution_function(metadata=False, param=False)
+def count(x):
+    """
+    Returns the number of unique values.
+
+    :param tuple x: Contains a tuple of values to be resolved.
+    :return: The number of unique values.
+    """
+    vals, = x
+    length = len(vals)
+    if length == 0:
+        return np.nan
+    return len(set(vals))
+
+
+@conflict_resolution_function(metadata=False, param=False)
+def choose_min(x):
+    """
+    Choose the smallest value.
+    :param tuple x: Contains a tuple of values to be resolved.
+    :return: The smallest value.
+    """
+    vals, = x
+    length = len(vals)
+    if length == 0:
+        return np.nan
+    return min(vals)
+
+
+@conflict_resolution_function(metadata=False, param=False)
+def choose_max(x):
+    """
+    Choose the largest value.
+    :param tuple x: Contains a tuple of values to be resolved.
+    :return: The largest value.
+    """
+    vals, = x
+    length = len(vals)
+    if length == 0:
+        return np.nan
+    return max(vals)
+
+
+@conflict_resolution_function(metadata=False, param=False)
+def choose_random(x):
+    """
+    Choose a random value.
+    :param tuple x: Contains a tuple of values to be resolved.
+    :return: A random value.
+    """
+    vals, = x
+    length = len(vals)
+    if length == 0:
+        return np.nan
+    random.choice(vals)
+
+
+@conflict_resolution_function(metadata=False, param=False)
+def vote(x):
+    """
+    Returns the most common element.
+    :param tuple x: Contains a tuple of values to be resolved.
+    :return:
+    """
+    vals, = x
+    length = len(vals)
+    if length == 0:
+        return np.nan
+    elif length == 1:
+        return vals[0]
+    counter = collections.Counter(vals)
+    return counter.most_common()[0][0]
+
+
+@conflict_resolution_function(metadata=False, param=False)
+def group(x):
+    """
+    Returns a set of all conflicting values.
+    :param tuple x: Contains a tuple of values to be resolved.
+    :return: A set of values.
+    """
+    vals, = x
+    return set(vals)
+
+
+@conflict_resolution_function(metadata=False, param=False)
+def no_gossip(x):
+    """
+    Returns values if all items in vals are equal, or np.nan
+    otherwise.
+
+    :param tuple x: Contains a tuple of values to be resolved.
+    :return: A canonical value or np.nan.
+    """
+    vals, = x
+    length = len(vals)
+    if length == 0:
+        return np.nan
+    elif length == 1:
+        return vals[0]
+    for i in vals:
+        if i != vals[0]:
+            # Short circuit on inconsistent value
+            return np.nan
+    # If no inconsistencies are seen, return the first value.
+    return vals[0]
+
+
+@conflict_resolution_function(metadata=False, param=True)
+def compute_metric(x):
+    """
+    Returns a numerical aggregation of values.
+
+    :param tuple x: Values to resolve, aggregation method.
+    :return: A numerical aggregation of vals.
+    """
+    vals, method = x
+    length = len(vals)
+    if length == 0:
+        return np.nan
+    if method == 'sum':
+        return sum(vals)
+    elif method == 'mean':
+        return statistics.mean(vals)
+    elif method == 'stdev':
+        return statistics.stdev(vals)
+    elif method == 'variance':
+        return statistics.variance(vals)
+
+
+@conflict_resolution_function(metadata=True, param=True)
+def choose(x):
+    """
+    Choose a value from a trusted source.
+
+    :param tuple x: Values to resolve, tuple of value sources, trusted source identifier.
+    :return: A trusted value.
+    """
+    vals, meta, trusted = x
+    length = len(vals)
+    if length == 0:
+        return np.nan
+    for i in range(len(vals)):
+        if meta[i] == trusted:
+            # Short circuit for trusted value
+            return vals[i]
+    # Return nan if no trusted value present
+    return np.nan
+
+
+@conflict_resolution_function(metadata=True, param=False)
+def annotated_concat(x):
+    """
+    Returns a collection of value/metadata pairs.
+
+    :param (tuple, tuple) x: Values to be resolved, and metadata values.
+    :return: A list of value/metadata tuples.
+    """
+    vals, meta = x
+    length = len(vals)
+    if length == 0:
+        return np.nan
+    seen = set()
+    concat = []
+    for i in range(len(vals)):
+        if vals[i] not in seen:
+            concat.append((vals[i], meta[i]))
+            seen.add(vals[i])
+        else:
+            continue
+    return concat
+
+
+@conflict_resolution_function(metadata=True, param=False)
+def minimize_metadata_value(x):
+    """
+    Chooses the value with the smallest corresponding metadata value.
+
+    :param (tuple, tuple) x: Values to be resolved, corresponding metadata.
+    :return: A canonical value.
+    """
+    vals, meta = x
+    length = len(vals)
+    if length == 0:
+        return np.nan
+    m = min(meta)
+    i = meta.index(m)
+    return vals[i]
+
+
+@conflict_resolution_function(metadata=True, param=False)
+def maximize_metadata_value(x):
+    """
+    Chooses the value with the largest corresponding metadata value.
+
+    :param (tuple, tuple) x: Values to be resolved, corresponding metadata.
+    :return: A canonical value.
+    """
+    vals, meta = x
+    length = len(vals)
+    if length == 0:
+        return np.nan
+    m = max(meta)
+    i = meta.index(m)
+    return vals[i]
--- recordlinkage/fuse.py
+++ recordlinkage/fuse.py
@@ -4,172 +4,58 @@ from __future__ import unicode_literals
 import random
 import warnings
 import collections
+import statistics
 import multiprocessing as mp
 import pandas as pd
-import recordlinkage as rl
+import numpy as np
 
-
-#######################################
-# Conflict Resolution Decorators
-#######################################
-
-def conflict_resolution_function(metadata=None, param=None):
-    """
-    Used to assert high-level requirements in the
-    conflict resolution process.
-
-    :param bool metadata: True/False
-    :param bool param: True/False
-    :return: function
-    """
-
-    def decorate(func):
-        func.metadata = metadata
-        func.param = param
-        return func
-
-    return decorate
+from recordlinkage.algorithms.conflict_resolution import (annotated_concat,
+                                                          choose,
+                                                          choose_max,
+                                                          choose_min,
+                                                          choose_random,
+                                                          compute_metric,
+                                                          count,
+                                                          group,
+                                                          maximize_metadata_value,
+                                                          minimize_metadata_value,
+                                                          no_gossip,
+                                                          vote)
 
 
 class FuseCore(object):
     def __init__(self):
         pass
 
-    def resolve(self, fun, vals, meta=None, **kwargs):
+    def resolve(self, fun, data):
         # Integrate values in vals (Series of tuples) using optional
         # meta (Series of tuples) by applying the conflict resolution function fun
         # to the series of tuples.
         pass
 
-    def queue_resolve(self, fun, vals, meta=None, **kwargs):
+    def queue_resolve(self, fun, c1, c2, m1=None, m2=None, trans_c=None, trans_m=None, **kwargs):
         # Integrate values in vals (Series of tuples) using optional
         # meta (Series of tuples) by applying the conflict resolution function fun
         # to the series of tuples.
         pass
 
-    def _format_resolve(self, c1, c2, m1=None, m2=None, trans_c=None, trans_m=None, memoize=False, *args, **kwargs):
+    def _format_resolve(self, c1, c2, m1=None, m2=None, trans_c=None, trans_m=None, **kwargs):
         # No implementation provided.
         # Override in subclass.
         pass
 
-    # Conflict Resolution Functions
-
-    @conflict_resolution_function(metadata=False, param=False)
-    def count(self, vals):
-        """
-        Returns the number of unique values.
-        :param tuple vals: Values to resolve.
-        :return: The number of unique values.
-        """
-        return len(set(vals))
-
-    @conflict_resolution_function(metadata=False, param=False)
-    def choose_min(self, vals):
-        """
-        Choose the smallest value.
-        :param tuple vals: Values to resolve.
-        :return: The smallest value.
-        """
-        return min(vals)
-
-    @conflict_resolution_function(metadata=False, param=False)
-    def choose_max(self, vals):
-        """
-        Choose the largest value.
-        :param tuple vals: Values to resolve.
-        :return: The largest value.
-        """
-        return max(vals)
-
-    @conflict_resolution_function(metadata=False, param=False)
-    def random(self, vals):
-        """
-        Choose a random value.
-        :param tuple vals: Values to resolve.
-        :return: A random value.
-        """
-        random.choice(vals)
-
-    @conflict_resolution_function(metadata=False, param=False)
-    def vote(self, vals):
-        """
-        Returns the most common element.
-        :param vals:
-        :return:
-        """
-        counter = collections.Counter(vals)
-        return counter.most_common()[0][0]
-
-    @conflict_resolution_function(metadata=False, param=False)
-    def group(self, vals):
-        pass
-
-    @conflict_resolution_function(metadata=False, param=False)
-    def shortest(self, vals):
-        pass
-
-    @conflict_resolution_function(metadata=False, param=False)
-    def longest(self, vals):
-        pass
-
-    @conflict_resolution_function(metadata=False, param=False)
-    def no_gossip(self, vals):
-        pass
-
-    @conflict_resolution_function(metadata=False, param=True)
-    def compute_metric(self, vals, method):
-        pass
-
-    @conflict_resolution_function(metadata=True, param=True)
-    def choose(self, vals, meta, trusted):
-        # Conflict resolution function
-        # One realization of trust_your_friends
-        pass
-
-    @conflict_resolution_function(metadata=True, param=False)
-    def annotated_concat(self, vals, meta):
-        pass
-
-    @conflict_resolution_function(metadata=True, param=False)
-    def minimize_metadata_value(self, vals, meta):
-        pass
-
-    @conflict_resolution_function(metadata=True, param=False)
-    def maximize_metadata_value(self, vals, meta):
-        pass
-
-    # @conflict_resolution_function
-    # @requires_metadata(True)
-    # def template_resolution_func(self, vals, meta):
-    #     """
-    #     Resolve values using optional metadata.
-    #     :param vals: A tuple of values
-    #     :param meta: An optional tuple of metadata
-    #     :return: A canonical value
-    #     """
-    #     # Assert invariants e.g. type checking
-    #     # Resolve conflicts / choose / generate value
-    #     # Return
-
     # Conflict Resolution Realizations
 
-    @conflict_resolution_realization
-    def trust_your_friends(self, c1, c2, trusted, method='choose'):
-        if method == 'choose':
-            return self.queue_resolve(self.choose, self._format_resolve(c1, c2, trust=trusted), trust=trusted)
-        elif method == 'highest_quality':
-            return self.queue_resolve(self.highest_quality, self._format_resolve(c1, c2, trust=trusted), trust=trusted)
-        else:
-            warnings.warn('Unrecognized method for trust_your_friends')
+    def trust_your_friends(self, c1, c2, trusted):
+        self.queue_resolve(choose, c1, c2, trust=trusted)
 
-    @conflict_resolution_realization
     def no_gossiping(self, c1, c2):
-        pass
+        self.queue_resolve(no_gossip, c1, c2)
 
-    @conflict_resolution_realization
-    def take_the_information(self, c1, c2):
-        pass
+    def roll_the_dice(self, c1, c2):
+        self.queue_resolve(choose_random, c1, c2)
 
+    # Turn collected metadata into a final result.
     def fuse(self, vectors, df_a, df_b, suffix_a='_a', suffix_b='_b'):
         # Apply refinements to vectors / index
         # Make calls to `resolve` using accumulated metadata
