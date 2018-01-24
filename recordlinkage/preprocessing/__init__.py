4e0d87f0d90b94b7bc022d395dcc3e5ea5d8b08e
Jonathan de Bruin <jonathandebruinhome@gmail.com>
2018-01-24 23:01:38 +0100

    Add docstring phonetic_algorithms attribute

--- recordlinkage/preprocessing/__init__.py
+++ recordlinkage/preprocessing/__init__.py
@@ -2,4 +2,6 @@ from recordlinkage.preprocessing.cleaning import *
 from recordlinkage.preprocessing.encoding import phonetic
 
 from recordlinkage.preprocessing.encoding import _list_phonetic_algorithms
+
 phonetic_algorithms = _list_phonetic_algorithms()
+"""List of available phonetic algorithms."""
