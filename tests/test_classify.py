c481789d3c8596a9420d3099a02a3c05c4051894
Jonathan de Bruin <jonathandebruinhome@gmail.com>
2018-07-16 23:12:33 +0200

    Use binary comparison vectors for tests

--- tests/test_classify.py
+++ tests/test_classify.py
@@ -4,8 +4,10 @@
 import pandas as pd
 import numpy as np
 from sklearn.exceptions import NotFittedError
+from sklearn.preprocessing import binarize
 
 import recordlinkage as rl
+from recordlinkage.datasets import binary_vectors
 import recordlinkage.config as cf
 
 import pytest
@@ -34,103 +36,112 @@ CLASSIFIERS = SUPERVISED_CLASSIFIERS + UNSUPERVISED_CLASSIFIERS
 
 
 N = 10000
-Y = pd.DataFrame(
-    np.random.random((N, 7)),
-    index=pd.MultiIndex.from_arrays(
-        [np.arange(0, N), np.arange(0, N)]),
-    columns=[
-        'name', 'second_name', 'surname', 'age', 'street', 'state',
-        'zipcode'
-    ])
-Y_TRAIN = Y.iloc[0:1000]
-
-MATCHES_ARRAY = np.random.randint(0, 2, N)
-MATCHES_SERIES = pd.Series(MATCHES_ARRAY, index=Y.index)
-MATCHES_INDEX = MATCHES_SERIES[MATCHES_SERIES == 1].index
 
 
 class TestClassifyData(object):
     @classmethod
     def setup_class(cls):
 
-        seed = 101
-        seed_gold = 1234  # noqa
+        cls.render_bin_test_data()
 
-        # set random state
-        np.random.seed(seed)
+    @classmethod
+    def render_bin_test_data(cls, n_pairs_train=5000, n_matches_train=1000,
+                             n_pairs_test=50000, n_matches_test=10000):
 
-        cls.y = pd.DataFrame(
-            np.random.random((N, 7)),
-            index=pd.MultiIndex.from_arrays(
-                [np.arange(0, N), np.arange(0, N)]),
-            columns=[
-                'name', 'second_name', 'surname', 'age', 'street', 'state',
-                'zipcode'
-            ])
+        cls.m = np.array([.92, .81, .85, .90, .99, .70, .56])
+        cls.u = np.array([.19, .23, .50, .11, .20, .14, .50])
 
-        cls.matches_array = np.random.randint(0, 2, N)
-        cls.matches_series = pd.Series(
-            cls.matches_array, index=cls.y.index)
-        cls.matches_index = cls.matches_series[cls.matches_series == 1].index
+        cls.labels = [
+            'name',
+            'second_name',
+            'surname',
+            'dob',
+            'street',
+            'state',
+            'zipcode'
+        ]
 
-        cls.y_train = cls.y.iloc[0:1000]
+        # Create the train dataset.
+        cls.X_train, cls.y_train = binary_vectors(
+            n_pairs_train,
+            n_matches_train,
+            m=cls.m,
+            u=cls.u,
+            random_state=535,
+            return_links=True)
+
+        cls.X_train.columns = cls.labels
+
+        # Create the test dataset.
+        cls.X_test, cls.y_test = binary_vectors(
+            n_pairs_test,
+            n_matches_test,
+            m=cls.m,
+            u=cls.u,
+            random_state=535,
+            return_links=True)
+
+        cls.y_test.columns = cls.labels
 
 
 class TestClassifyAPI(TestClassifyData):
 
-    @pytest.mark.parametrize('classifier', SUPERVISED_CLASSIFIERS)
+    @pytest.mark.parametrize('classifier', CLASSIFIERS)
     def test_return_result_options(self, classifier):
 
         cl = classifier()
-        cl.fit(self.y, self.matches_index)
+        if isinstance(cl, tuple(UNSUPERVISED_CLASSIFIERS)):
+            cl.fit(self.X_train)
+        else:
+            cl.fit(self.X_train, self.y_train)
 
-        prediction_default = cl.predict(self.y)
+        prediction_default = cl.predict(self.X_test)
         assert isinstance(prediction_default, pd.MultiIndex)
 
         with cf.option_context('classification.return_type', 'index'):
-            prediction_multiindex = cl.predict(comparison_vectors=self.y)
+            prediction_multiindex = cl.predict(comparison_vectors=self.X_train)
             assert isinstance(prediction_multiindex, pd.MultiIndex)
 
         with cf.option_context('classification.return_type', 'array'):
-            prediction_ndarray = cl.predict(comparison_vectors=self.y)
+            prediction_ndarray = cl.predict(comparison_vectors=self.X_train)
             assert isinstance(prediction_ndarray, np.ndarray)
 
         with cf.option_context('classification.return_type', 'series'):
-            prediction_series = cl.predict(comparison_vectors=self.y)
+            prediction_series = cl.predict(comparison_vectors=self.X_train)
             assert isinstance(prediction_series, pd.Series)
 
         with pytest.raises(ValueError):
             with cf.option_context('classification.return_type',
                                    'unknown_return_type'):
                 cl.predict(
-                    comparison_vectors=self.y
+                    comparison_vectors=self.X_train
                 )
 
     @pytest.mark.parametrize('classifier', SUPERVISED_CLASSIFIERS)
     def test_return_result_options_depr(self, classifier):
 
         cl = classifier()
-        cl.fit(self.y, self.matches_index)
+        cl.fit(self.X_train, self.y_train)
 
-        prediction_default = cl.predict(self.y)
+        prediction_default = cl.predict(self.X_test)
         assert isinstance(prediction_default, pd.MultiIndex)
 
         prediction_multiindex = cl.predict(
-            comparison_vectors=self.y, return_type='index')
+            comparison_vectors=self.X_train, return_type='index')
         assert isinstance(prediction_multiindex, pd.MultiIndex)
 
         prediction_ndarray = cl.predict(
-            comparison_vectors=self.y, return_type='array')
+            comparison_vectors=self.X_train, return_type='array')
         assert isinstance(prediction_ndarray, np.ndarray)
 
         prediction_series = cl.predict(
-            comparison_vectors=self.y,
+            comparison_vectors=self.X_train,
             return_type='series')
         assert isinstance(prediction_series, pd.Series)
 
         with pytest.raises(ValueError):
             cl.predict(
-                comparison_vectors=self.y,
+                comparison_vectors=self.X_train,
                 return_type='unknown_return_type'
             )
 
@@ -140,11 +151,11 @@ class TestClassifyAPI(TestClassifyData):
         cl = classifier()
 
         if isinstance(cl, tuple(UNSUPERVISED_CLASSIFIERS)):
-            cl.fit(self.y)
+            cl.fit(self.X_train)
         else:
-            cl.fit(self.y, self.matches_index)
+            cl.fit(self.X_train, self.y_train)
 
-        probs = cl.prob(self.y)
+        probs = cl.prob(self.X_test)
         print(probs)
 
         assert isinstance(probs, pd.Series)
@@ -152,31 +163,17 @@ class TestClassifyAPI(TestClassifyData):
         assert probs.max() <= 1.0
         assert probs.min() >= 0.0
 
-    @pytest.mark.parametrize('classifier', CLASSIFIERS)
-    def test_predict(self, classifier):
-
-        cl = classifier()
-
-        if isinstance(cl, tuple(UNSUPERVISED_CLASSIFIERS)):
-            cl.fit(self.y)
-        else:
-            cl.fit(self.y, self.matches_index)
-
-        expected = cl.predict(self.y)
-
-        assert isinstance(expected, pd.MultiIndex)
-
     @pytest.mark.parametrize('classifier', UNSUPERVISED_CLASSIFIERS)
     def test_fit_predict_unsupervised(self, classifier):
 
         cl = classifier()
-        cl.fit(self.y_train)
-        result = cl.predict(self.y_train)
+        cl.fit(self.X_train)
+        result = cl.predict(self.X_train)
 
         assert isinstance(result, pd.MultiIndex)
 
         cl2 = classifier()
-        expected = cl2.fit_predict(self.y_train)
+        expected = cl2.fit_predict(self.X_train)
 
         assert isinstance(expected, pd.MultiIndex)
         assert result.values.shape == expected.values.shape
@@ -187,13 +184,13 @@ class TestClassifyAPI(TestClassifyData):
     def test_fit_predict_supervised(self, classifier):
 
         cl = classifier()
-        cl.fit(self.y_train, self.matches_index)
-        result = cl.predict(self.y_train)
+        cl.fit(self.X_train, self.y_train)
+        result = cl.predict(self.X_train)
 
         assert isinstance(result, pd.MultiIndex)
 
         cl2 = classifier()
-        expected = cl2.fit_predict(self.y_train, self.matches_index)
+        expected = cl2.fit_predict(self.X_train, self.y_train)
 
         assert isinstance(expected, pd.MultiIndex)
         assert result.values.shape == expected.values.shape
@@ -206,7 +203,7 @@ class TestClassifyAPI(TestClassifyData):
         cl = classifier()
 
         with pytest.raises(NotFittedError):
-            cl.predict(self.y)
+            cl.predict(self.X_test)
 
     @pytest.mark.parametrize('classifier', SUPERVISED_CLASSIFIERS)
     def test_fit_empty_frame_supervised(self, classifier):
@@ -215,8 +212,8 @@ class TestClassifyAPI(TestClassifyData):
 
         with pytest.raises(ValueError):
             cl.fit(
-                pd.DataFrame(columns=self.y_train.columns),
-                self.matches_index
+                pd.DataFrame(columns=self.X_train.columns),
+                self.y_train
             )
 
     @pytest.mark.parametrize('classifier', UNSUPERVISED_CLASSIFIERS)
@@ -225,26 +222,26 @@ class TestClassifyAPI(TestClassifyData):
         cl = classifier()
 
         with pytest.raises(ValueError):
-            cl.fit(pd.DataFrame(columns=self.y_train.columns))
+            cl.fit(pd.DataFrame(columns=self.X_train.columns))
 
 
-class TestKMeansAlgorithms(TestClassifyData):
+class TestKMeans(TestClassifyData):
 
     def test_kmeans(self):
 
         kmeans = rl.KMeansClassifier()
-        kmeans.fit(self.y_train)
-        result = kmeans.predict(self.y_train)
+        kmeans.fit(self.X_train)
+        result = kmeans.predict(self.X_test)
 
         assert isinstance(result, pd.MultiIndex)
-        assert result.shape[0] == 519
+        assert result.shape[0] == 11670
 
     def test_kmean_parameters(self):
 
         kmeans = rl.KMeansClassifier()
-        kmeans.fit(self.y_train)
+        kmeans.fit(self.X_train)
 
-        _, n_features = self.y_train.shape
+        _, n_features = self.X_train.shape
 
         assert isinstance(kmeans.match_cluster_center, np.ndarray)
         assert kmeans.match_cluster_center.shape == (n_features,)
@@ -255,19 +252,19 @@ class TestKMeansAlgorithms(TestClassifyData):
     def test_kmeans_error(self):
 
         kmeans = rl.KMeansClassifier()
-        kmeans.fit(self.y_train)
+        kmeans.fit(self.X_train)
 
         # There are no probabilities
         with pytest.raises(AttributeError):
-            kmeans.prob(self.y)
+            kmeans.prob(self.X_train)
 
     def test_kmeans_manual(self):
         """KMeansClassifier with manual cluster centers"""
 
         # Make random test data.
         np.random.seed(535)
-        manual_mcc = list(np.random.randn(self.y_train.shape[1]))
-        manual_nmcc = list(np.random.randn(self.y_train.shape[1]))
+        manual_mcc = list(np.random.randn(self.X_train.shape[1]))
+        manual_nmcc = list(np.random.randn(self.X_train.shape[1]))
 
         # Initialize the KMeansClassifier
         kmeans = rl.KMeansClassifier()
@@ -281,7 +278,7 @@ class TestKMeansAlgorithms(TestClassifyData):
         kmeans.nonmatch_cluster_center = manual_nmcc
 
         # Perform the prediction
-        kmeans.predict(self.y)
+        kmeans.predict(self.X_test)
 
         # Check the match clusters
         mcc = kmeans.match_cluster_center
@@ -290,21 +287,21 @@ class TestKMeansAlgorithms(TestClassifyData):
         assert_almost_equal(nmcc, manual_nmcc)
 
 
-class TestClassifyAlgorithms(TestClassifyData):
+class TestLogistic(TestClassifyData):
     def test_logistic_regression_basic(self):
 
         logis = rl.LogisticRegressionClassifier()
 
         # Test the basics
-        logis.fit(self.y_train, self.matches_index)
-        logis.predict(self.y)
-        logis.prob(self.y)
+        logis.fit(self.X_train, self.y_train)
+        logis.predict(self.X_test)
+        logis.prob(self.X_train)
 
     def test_logistic_regression_manual(self):
 
         # Make random test data.
         np.random.seed(535)
-        manual_coefficients = np.random.randn(self.y_train.shape[1])
+        manual_coefficients = np.random.randn(self.X_train.shape[1])
         manual_intercept = np.random.rand()
 
         # Initialize the LogisticRegressionClassifier
@@ -317,41 +314,44 @@ class TestClassifyAlgorithms(TestClassifyData):
         logis.intercept = manual_intercept
 
         # Perform the prediction
-        logis.predict(self.y)
+        logis.predict(self.X_test)
 
         # Train the classifier after manual setting
-        logis.fit(self.y_train, self.matches_index)
-        logis.predict(self.y)
+        logis.fit(self.X_train, self.y_train)
+        logis.predict(self.X_test)
 
         lc = logis.coefficients
-        assert lc.shape == (self.y_train.shape[1], )
+        assert lc.shape == (self.X_train.shape[1], )
         assert isinstance(logis.intercept, (float))
+
+
+class TestSVM(TestClassifyData):
 
     def test_svm(self):
 
         svm = rl.SVMClassifier()
-        svm.fit(self.y_train, self.matches_index)
-        svm.predict(self.y)
+        svm.fit(self.X_train, self.y_train)
+        svm.predict(self.X_test)
 
         # There are no probabilities
         with pytest.raises(AttributeError):
-            svm.prob(self.y)
+            svm.prob(self.X_train)
 
 
-class TestClassifyECM(TestClassifyData):
+class TestECM(TestClassifyData):
 
     def test_ecm_probs(self):
 
         ecm = rl.ECMClassifier()
-        ecm.fit(self.y_train.round())
+        ecm.fit(self.X_train.round())
 
         assert (ecm.p <= 1.0) & (ecm.p >= 0.0)
 
     def test_ecm_predict(self):
 
         ecm = rl.ECMClassifier()
-        ecm.fit(self.y.round())
-        prediction = ecm.predict(self.y.round())
+        ecm.fit(self.X_train.round())
+        prediction = ecm.predict(self.X_test)
 
 
 class TestFellegiSunter(TestClassifyData):
@@ -360,68 +360,77 @@ class TestFellegiSunter(TestClassifyData):
         rl.NaiveBayesClassifier,
         rl.ECMClassifier
     ])
-    def test_FS_parameters(self, classifier):
+    def test_fs_parameters(self, classifier):
 
         cl = classifier()
         if isinstance(cl, tuple(UNSUPERVISED_CLASSIFIERS)):
-            cl.fit(Y_TRAIN)
+            cl.fit(self.X_train)
         else:
-            cl.fit(Y_TRAIN, MATCHES_INDEX)
+            cl.fit(self.X_train, self.y_train)
 
         # p
         assert np.isscalar(cl.p)
         assert np.exp(cl.log_p) == cl.p
 
         # m
-        assert isinstance(cl.m_probs, np.ndarray)
-        assert cl.m_probs.shape == (Y_TRAIN.shape[1],)
-        assert_almost_equal(np.exp(cl.log_m_probs), cl.m_probs)
+        assert isinstance(cl.m_probs, dict)
+        assert len(cl.m_probs.keys()) == self.X_train.shape[1]
+        for col, value in cl.m_probs.items():
+            for key, out in value.items():
+                assert_almost_equal(
+                    np.exp(cl.log_m_probs[col][key]),
+                    cl.m_probs[col][key]
+                )
 
         # u
-        assert isinstance(cl.u_probs, np.ndarray)
-        assert cl.u_probs.shape == (Y_TRAIN.shape[1],)
-        assert_almost_equal(np.exp(cl.log_u_probs), cl.u_probs)
+        assert isinstance(cl.u_probs, dict)
+        assert len(cl.u_probs.keys()) == self.X_train.shape[1]
+        for col, value in cl.u_probs.items():
+            for key, out in value.items():
+                assert_almost_equal(
+                    np.exp(cl.log_u_probs[col][key]),
+                    cl.u_probs[col][key]
+                )
+    # @pytest.mark.parametrize('classifier', [
+    #     rl.NaiveBayesClassifier,
+    #     rl.ECMClassifier
+    # ])
+    # def test_fs_parameters_set_get(self, classifier):
 
-    @pytest.mark.parametrize('classifier', [
-        rl.NaiveBayesClassifier,
-        rl.ECMClassifier
-    ])
-    def test_FS_parameters_set_get(self, classifier):
+    #     # there were some issues with setting and getting parameters. Afters
+    #     # getting parameters, the internel parameters were messed up.
 
-        # there were some issues with setting and getting parameters. Afters
-        # getting parameters, the internel parameters were messed up.
+    #     cl = classifier()
+    #     if isinstance(cl, tuple(UNSUPERVISED_CLASSIFIERS)):
+    #         cl.fit(Y_TRAIN)
+    #     else:
+    #         cl.fit(Y_TRAIN, MATCHES_INDEX)
 
-        cl = classifier()
-        if isinstance(cl, tuple(UNSUPERVISED_CLASSIFIERS)):
-            cl.fit(Y_TRAIN)
-        else:
-            cl.fit(Y_TRAIN, MATCHES_INDEX)
+    #     probs_before = cl.prob(Y_TRAIN)
+    #     predict_before = cl.predict(Y_TRAIN)
 
-        probs_before = cl.prob(Y_TRAIN)
-        predict_before = cl.predict(Y_TRAIN)
+    #     # p
+    #     attributes = ["p", "log_p",
+    #                   "m_probs", "u_probs",
+    #                   "log_m_probs", "log_u_probs",
+    #                   "weights", "log_weights"]
 
-        # p
-        attributes = ["p", "log_p",
-                      "m_probs", "u_probs",
-                      "log_m_probs", "log_u_probs",
-                      "weights", "log_weights"]
+    #     for attr in attributes:
+    #         print(attr)
+    #         value = getattr(cl, attr)
 
-        for attr in attributes:
-            print(attr)
-            value = getattr(cl, attr)
+    #         if attr not in ["weights", "log_weights"]:
+    #             setattr(cl, attr, value)
 
-            if attr not in ["weights", "log_weights"]:
-                setattr(cl, attr, value)
+    #     probs_after = cl.prob(Y_TRAIN)
+    #     predict_after = cl.predict(Y_TRAIN)
 
-        probs_after = cl.prob(Y_TRAIN)
-        predict_after = cl.predict(Y_TRAIN)
-
-        ptm.assert_series_equal(probs_before, probs_after)
-        ptm.assert_index_equal(predict_before, predict_after)
+    #     ptm.assert_series_equal(probs_before, probs_after)
+    #     ptm.assert_index_equal(predict_before, predict_after)
 
     # def test_FS_supervised_binarize(self):
 
     #     cl = rl.NaiveBayesClassifier(binarize=None)
-    #     cl.fit(self.y_train, self.matches_index)
-    #     cl.predict(self.y)
-    #     cl.prob(self.y)
+    #     cl.fit(self.X_train, self.y_train)
+    #     cl.predict(self.X_test)
+    #     cl.prob(self.X_train)
