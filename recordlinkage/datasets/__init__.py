833ee09c0cd13bdeba246130fa2d36b43eb316b3
Jonathan de Bruin <jonathandebruinos@gmail.com>
2019-03-29 20:38:26 +0100

    Restrict interface for external datasets

--- recordlinkage/datasets/__init__.py
+++ recordlinkage/datasets/__init__.py
@@ -1,3 +1,5 @@
 from recordlinkage.datasets.external import *
 from recordlinkage.datasets.febrl import *
-from recordlinkage.datasets.generate import *
+from recordlinkage.datasets.generate import clear_data_home
+from recordlinkage.datasets.generate import get_data_home
+from recordlinkage.datasets.generate import load_krebsregister
