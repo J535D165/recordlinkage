e254c638ce29fefe60b404b5ba18922f939ccefd
Tomasz Walen <Tomasz.Walen@ing.com>
2022-04-07 20:32:33 +0200

    fixing index operations to get rid of pandas deprecation warnings

--- recordlinkage/measures.py
+++ recordlinkage/measures.py
@@ -144,7 +144,7 @@ def true_positives(links_true, links_pred):
     links_true = _get_multiindex(links_true)
     links_pred = _get_multiindex(links_pred)
 
-    return len(links_true & links_pred)
+    return len(links_true.intersection(links_pred))
 
 
 def true_negatives(links_true, links_pred, total):
@@ -176,7 +176,7 @@ def true_negatives(links_true, links_pred, total):
     if isinstance(total, pandas.MultiIndex):
         total = len(total)
 
-    return int(total) - len(links_true | links_pred)
+    return int(total) - len(links_true.union(links_pred))
 
 
 def false_positives(links_true, links_pred):
