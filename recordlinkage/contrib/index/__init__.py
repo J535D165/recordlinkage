b6f3f1e8e53cd9b4d692a1726eb6cf52d9fb2b64
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-07-19 15:15:54 +0200

    Fix importerror

--- recordlinkage/contrib/index/__init__.py
+++ recordlinkage/contrib/index/__init__.py
@@ -23,3 +23,10 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
+
+
+from recordlinkage.contrib.index.neighbourhoodblock.neighbourhoodblock import (
+    NeighbourhoodBlock,
+)
+
+__all__ = ["NeighbourhoodBlock"]
